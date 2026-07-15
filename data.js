ebff6697beeac1a16566b8fdba3c60f4a5eda04e
FleterosLP-ELEBES <lucasmendez19@gmail.com>
2026-07-15 10:04:45 -0300

    Publicacion inicial: data.js

---
+++ data.js
@@ -0,0 +1,115 @@
+/* GENERADO AUTOMATICAMENTE por robot-actualizar-web.ps1 - NO EDITAR A MANO
+   Ultima actualizacion: 2026-07-15 09:54 */
+window.__LPE_DATA__ = { registros: [
+ {"fecha":"2026-07-01","fletero":"Diego Zacarias","repartos":1,"boletas":19,"entregadas":17}
+,{"fecha":"2026-07-01","fletero":"Emiliano Juan Amarilla","repartos":1,"boletas":36,"entregadas":35}
+,{"fecha":"2026-07-01","fletero":"Marcos Sanchez","repartos":1,"boletas":34,"entregadas":32}
+,{"fecha":"2026-07-01","fletero":"Miguel Angel Pallini","repartos":1,"boletas":47,"entregadas":44}
+,{"fecha":"2026-07-01","fletero":"Walter Tenore","repartos":1,"boletas":46,"entregadas":41}
+,{"fecha":"2026-07-02","fletero":"Claudio Garcia","repartos":1,"boletas":19,"entregadas":16}
+,{"fecha":"2026-07-02","fletero":"Diego Zacarias","repartos":1,"boletas":9,"entregadas":9}
+,{"fecha":"2026-07-02","fletero":"Emiliano Juan Amarilla","repartos":1,"boletas":42,"entregadas":41}
+,{"fecha":"2026-07-02","fletero":"Juan Escoto","repartos":1,"boletas":1,"entregadas":1}
+,{"fecha":"2026-07-02","fletero":"Marcos Sanchez","repartos":1,"boletas":25,"entregadas":23}
+,{"fecha":"2026-07-02","fletero":"Miguel Angel Pallini","repartos":1,"boletas":42,"entregadas":41}
+,{"fecha":"2026-07-02","fletero":"Walter Janiuk","repartos":1,"boletas":26,"entregadas":18}
+,{"fecha":"2026-07-02","fletero":"Walter Tenore","repartos":1,"boletas":43,"entregadas":39}
+,{"fecha":"2026-07-03","fletero":"Claudio Garcia","repartos":1,"boletas":28,"entregadas":22}
+,{"fecha":"2026-07-03","fletero":"Diego Zacarias","repartos":1,"boletas":32,"entregadas":31}
+,{"fecha":"2026-07-03","fletero":"Emiliano Juan Amarilla","repartos":1,"boletas":44,"entregadas":42}
+,{"fecha":"2026-07-03","fletero":"Ivan Montoya","repartos":1,"boletas":25,"entregadas":23}
+,{"fecha":"2026-07-03","fletero":"Marcos Sanchez","repartos":1,"boletas":34,"entregadas":29}
+,{"fecha":"2026-07-03","fletero":"Miguel Angel Pallini","repartos":1,"boletas":46,"entregadas":39}
+,{"fecha":"2026-07-03","fletero":"Nahuel Montoya","repartos":1,"boletas":46,"entregadas":34}
+,{"fecha":"2026-07-03","fletero":"Simon Olivera","repartos":1,"boletas":45,"entregadas":36}
+,{"fecha":"2026-07-03","fletero":"Walter Tenore","repartos":1,"boletas":25,"entregadas":24}
+,{"fecha":"2026-07-06","fletero":"Claudio Garcia","repartos":1,"boletas":11,"entregadas":9}
+,{"fecha":"2026-07-06","fletero":"Diego Zacarias","repartos":1,"boletas":15,"entregadas":13}
+,{"fecha":"2026-07-06","fletero":"Emiliano Juan Amarilla","repartos":1,"boletas":44,"entregadas":42}
+,{"fecha":"2026-07-06","fletero":"Hernan Rivero","repartos":1,"boletas":43,"entregadas":32}
+,{"fecha":"2026-07-06","fletero":"Juan Escoto","repartos":1,"boletas":33,"entregadas":26}
+,{"fecha":"2026-07-06","fletero":"Leonardo Santa Cruz","repartos":1,"boletas":71,"entregadas":33}
+,{"fecha":"2026-07-06","fletero":"Marcos Sanchez","repartos":1,"boletas":24,"entregadas":22}
+,{"fecha":"2026-07-06","fletero":"Mariano Sosa","repartos":1,"boletas":37,"entregadas":24}
+,{"fecha":"2026-07-06","fletero":"Miguel Angel Pallini","repartos":1,"boletas":42,"entregadas":37}
+,{"fecha":"2026-07-06","fletero":"Walter Tenore","repartos":1,"boletas":55,"entregadas":54}
+,{"fecha":"2026-07-07","fletero":"Claudio Garcia","repartos":1,"boletas":37,"entregadas":27}
+,{"fecha":"2026-07-07","fletero":"Diego Zacarias","repartos":1,"boletas":15,"entregadas":15}
+,{"fecha":"2026-07-07","fletero":"Emiliano Juan Amarilla","repartos":1,"boletas":28,"entregadas":27}
+,{"fecha":"2026-07-07","fletero":"Ivan Montoya","repartos":1,"boletas":44,"entregadas":35}
+,{"fecha":"2026-07-07","fletero":"Juan Escoto","repartos":1,"boletas":32,"entregadas":32}
+,{"fecha":"2026-07-07","fletero":"Marcos Sanchez","repartos":1,"boletas":22,"entregadas":20}
+,{"fecha":"2026-07-07","fletero":"Miguel Angel Pallini","repartos":1,"boletas":34,"entregadas":26}
+,{"fecha":"2026-07-07","fletero":"Nahuel Montoya","repartos":1,"boletas":29,"entregadas":29}
+,{"fecha":"2026-07-07","fletero":"Simon Olivera","repartos":1,"boletas":46,"entregadas":29}
+,{"fecha":"2026-07-07","fletero":"Walter Janiuk","repartos":1,"boletas":55,"entregadas":49}
+,{"fecha":"2026-07-07","fletero":"Walter Tenore","repartos":1,"boletas":38,"entregadas":37}
+,{"fecha":"2026-07-08","fletero":"Diego Zacarias","repartos":1,"boletas":14,"entregadas":11}
+,{"fecha":"2026-07-08","fletero":"Emiliano Juan Amarilla","repartos":1,"boletas":27,"entregadas":26}
+,{"fecha":"2026-07-08","fletero":"Hernan Rivero","repartos":1,"boletas":39,"entregadas":35}
+,{"fecha":"2026-07-08","fletero":"Leonardo Santa Cruz","repartos":1,"boletas":53,"entregadas":42}
+,{"fecha":"2026-07-08","fletero":"Marcos Sanchez","repartos":1,"boletas":13,"entregadas":11}
+,{"fecha":"2026-07-08","fletero":"Mariano Sosa","repartos":1,"boletas":44,"entregadas":38}
+,{"fecha":"2026-07-08","fletero":"Miguel Angel Pallini","repartos":1,"boletas":29,"entregadas":29}
+,{"fecha":"2026-07-08","fletero":"Nahuel Montoya","repartos":1,"boletas":19,"entregadas":17}
+,{"fecha":"2026-07-08","fletero":"Walter Tenore","repartos":1,"boletas":27,"entregadas":27}
+,{"fecha":"2026-07-09","fletero":"Diego Zacarias","repartos":1,"boletas":27,"entregadas":25}
+,{"fecha":"2026-07-09","fletero":"Emiliano Juan Amarilla","repartos":1,"boletas":33,"entregadas":32}
+,{"fecha":"2026-07-09","fletero":"Marcos Sanchez","repartos":1,"boletas":18,"entregadas":18}
+,{"fecha":"2026-07-09","fletero":"Miguel Angel Pallini","repartos":1,"boletas":32,"entregadas":32}
+,{"fecha":"2026-07-09","fletero":"Walter Tenore","repartos":1,"boletas":53,"entregadas":53}
+,{"fecha":"2026-07-10","fletero":"Claudio Garcia","repartos":1,"boletas":21,"entregadas":20}
+,{"fecha":"2026-07-10","fletero":"Ivan Montoya","repartos":1,"boletas":42,"entregadas":39}
+,{"fecha":"2026-07-10","fletero":"Juan Escoto","repartos":1,"boletas":36,"entregadas":34}
+,{"fecha":"2026-07-10","fletero":"Mariano Sosa","repartos":1,"boletas":1,"entregadas":1}
+,{"fecha":"2026-07-10","fletero":"Nahuel Montoya","repartos":1,"boletas":59,"entregadas":54}
+,{"fecha":"2026-07-10","fletero":"Simon Olivera","repartos":1,"boletas":47,"entregadas":41}
+,{"fecha":"2026-07-10","fletero":"Walter Janiuk","repartos":2,"boletas":90,"entregadas":72}
+,{"fecha":"2026-07-11","fletero":"Claudio Garcia","repartos":1,"boletas":29,"entregadas":25}
+,{"fecha":"2026-07-11","fletero":"Diego Zacarias","repartos":1,"boletas":33,"entregadas":31}
+,{"fecha":"2026-07-11","fletero":"Hernan Rivero","repartos":1,"boletas":45,"entregadas":34}
+,{"fecha":"2026-07-11","fletero":"Ivan Montoya","repartos":1,"boletas":51,"entregadas":43}
+,{"fecha":"2026-07-11","fletero":"Juan Escoto","repartos":1,"boletas":19,"entregadas":18}
+,{"fecha":"2026-07-11","fletero":"Leonardo Santa Cruz","repartos":1,"boletas":35,"entregadas":33}
+,{"fecha":"2026-07-11","fletero":"Luciano Gasparini","repartos":1,"boletas":22,"entregadas":21}
+,{"fecha":"2026-07-11","fletero":"Marcos Sanchez","repartos":1,"boletas":42,"entregadas":37}
+,{"fecha":"2026-07-11","fletero":"Miguel Angel Pallini","repartos":1,"boletas":44,"entregadas":43}
+,{"fecha":"2026-07-11","fletero":"Walter Tenore","repartos":1,"boletas":66,"entregadas":60}
+,{"fecha":"2026-07-13","fletero":"Diego Zacarias","repartos":1,"boletas":32,"entregadas":31}
+,{"fecha":"2026-07-13","fletero":"Emiliano Juan Amarilla","repartos":1,"boletas":42,"entregadas":41}
+,{"fecha":"2026-07-13","fletero":"Juan Escoto","repartos":1,"boletas":36,"entregadas":32}
+,{"fecha":"2026-07-13","fletero":"Leonardo Santa Cruz","repartos":1,"boletas":27,"entregadas":23}
+,{"fecha":"2026-07-13","fletero":"Marcos Sanchez","repartos":1,"boletas":22,"entregadas":20}
+,{"fecha":"2026-07-13","fletero":"Mariano Sosa","repartos":1,"boletas":29,"entregadas":22}
+,{"fecha":"2026-07-13","fletero":"Miguel Angel Pallini","repartos":1,"boletas":34,"entregadas":32}
+,{"fecha":"2026-07-13","fletero":"Nahuel Montoya","repartos":2,"boletas":64,"entregadas":58}
+,{"fecha":"2026-07-13","fletero":"Simon Olivera","repartos":1,"boletas":43,"entregadas":43}
+,{"fecha":"2026-07-13","fletero":"Walter Janiuk","repartos":1,"boletas":13,"entregadas":13}
+,{"fecha":"2026-07-13","fletero":"Walter Tenore","repartos":1,"boletas":23,"entregadas":20}
+,{"fecha":"2026-07-14","fletero":"Claudio Garcia","repartos":1,"boletas":52,"entregadas":39}
+,{"fecha":"2026-07-14","fletero":"Diego Zacarias","repartos":1,"boletas":28,"entregadas":22}
+,{"fecha":"2026-07-14","fletero":"Emiliano Juan Amarilla","repartos":1,"boletas":23,"entregadas":22}
+,{"fecha":"2026-07-14","fletero":"Hernan Rivero","repartos":1,"boletas":36,"entregadas":32}
+,{"fecha":"2026-07-14","fletero":"Leonardo Santa Cruz","repartos":1,"boletas":31,"entregadas":31}
+,{"fecha":"2026-07-14","fletero":"Marcos Sanchez","repartos":1,"boletas":22,"entregadas":19}
+,{"fecha":"2026-07-14","fletero":"Mariano Sosa","repartos":1,"boletas":46,"entregadas":43}
+,{"fecha":"2026-07-14","fletero":"Miguel Angel Pallini","repartos":1,"boletas":43,"entregadas":40}
+,{"fecha":"2026-07-14","fletero":"Nahuel Montoya","repartos":1,"boletas":26,"entregadas":26}
+,{"fecha":"2026-07-14","fletero":"Walter Janiuk","repartos":1,"boletas":52,"entregadas":52}
+,{"fecha":"2026-07-14","fletero":"Walter Tenore","repartos":1,"boletas":19,"entregadas":17}
+,{"fecha":"2026-07-15","fletero":"Claudio Garcia","repartos":1,"boletas":33,"entregadas":33}
+,{"fecha":"2026-07-15","fletero":"Diego Zacarias","repartos":1,"boletas":15,"entregadas":15}
+,{"fecha":"2026-07-15","fletero":"Emiliano Juan Amarilla","repartos":1,"boletas":15,"entregadas":15}
+,{"fecha":"2026-07-15","fletero":"Hernan Rivero","repartos":1,"boletas":1,"entregadas":1}
+,{"fecha":"2026-07-15","fletero":"Ivan Montoya","repartos":1,"boletas":40,"entregadas":40}
+,{"fecha":"2026-07-15","fletero":"Juan Escoto","repartos":1,"boletas":20,"entregadas":20}
+,{"fecha":"2026-07-15","fletero":"Leonardo Santa Cruz","repartos":1,"boletas":37,"entregadas":37}
+,{"fecha":"2026-07-15","fletero":"Marcos Sanchez","repartos":1,"boletas":21,"entregadas":21}
+,{"fecha":"2026-07-15","fletero":"Mariano Sosa","repartos":1,"boletas":40,"entregadas":40}
+,{"fecha":"2026-07-15","fletero":"Miguel Angel Pallini","repartos":1,"boletas":13,"entregadas":13}
+,{"fecha":"2026-07-15","fletero":"Nahuel Montoya","repartos":1,"boletas":47,"entregadas":47}
+,{"fecha":"2026-07-15","fletero":"Simon Olivera","repartos":1,"boletas":27,"entregadas":27}
+,{"fecha":"2026-07-15","fletero":"Walter Janiuk","repartos":1,"boletas":2,"entregadas":2}
+,{"fecha":"2026-07-15","fletero":"Walter Tenore","repartos":1,"boletas":19,"entregadas":19}
+] };
+window.__LPE_DATA__.empresas = [{"id":"lagopuelo","nombre":"Lago Puelo S.A.","logo":"assets/img/logo-lagopuelo.jpg","boletas":1446,"rechazadas":194},{"id":"elebes","nombre":"Elebes S.A.","logo":"assets/img/logo-elebes.jpg","boletas":1865,"rechazadas":186}];
